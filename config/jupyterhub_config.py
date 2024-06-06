82e0ef78aa986db374dd8d8b952257fb1a046303
Tao Chen <tachen@phys.ethz.ch>
2024-06-06 19:59:00 +0200

    clean up

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -1,13 +1,16 @@
 import os
 import sys
+
+# The following is necessary
+#  ==============================================================================
 CONFIG_SCRIPT_DIR = os.getenv('CONFIG_SCRIPT_DIR', '/config')
 CONFIG_FILE = os.getenv('CONFIG_FILE', '/etc/jupyterhub/config.yaml')
-# CONFIG_SCRIPT_DIR='/etc/jupyterhub'
 sys.path.append(CONFIG_SCRIPT_DIR)
 from MyConfig import MyConfig,my_pre_spawn_hook
 from MyOAuth import MyOAuth
 from MyDockerSpawner import MyDockerSpawner
 import docker
+# ==============================================================================
 c = get_config()
 
 my_config=MyConfig(filename=CONFIG_FILE)
@@ -20,24 +23,27 @@ c.Spawner.default_url = '/lab'
 # Authenticator
 c.JupyterHub.authenticator_class = MyOAuth
 c.MyOAuth.auto_login = True
-c.MyOAuth.discovery_url = "https://auth.xxxxxxx.com/.well-known/openid-configuration"
+c.MyOAuth.discovery_url = "https://auth.example.com/realms/realm"+"/.well-known/openid-configuration"
+# or (you can add "/.well-known/openid-configuration" or not)
+c.MyOAuth.discovery_url = "https://auth.example.com/realms/realm"
 c.MyOAuth.client_id = "code-oidc"
 c.MyOAuth.client_secret = "CHANGEIT"
+c.MyOAuth.scope = ['openid', 'profile', 'email', 'groups']
 c.MyOAuth.oauth_callback_url = "https://code.example.com/hub/oauth_callback"
 c.MyOAuth.username_claim = "preferred_username"
 c.MyOAuth.login_service = 'Example'
-
-GROUP = ['members/students/phd', 'members/students/msc', 'members/staff/postdoc', 'members/staff/', 'admins']
+# If group is not defined, all users will be allowed
+GROUP = ['group1', 'group2', 'admins']
 c.MyOAuth.allowed_groups = GROUP
 # users with `administrator` role will be marked as admin
 c.MyOAuth.admin_groups = ['Administrators', 'AdminJupyterHub','admins']
 c.MyOAuth.manage_groups = False
-c.MyOAuth.scope = ['openid', 'profile', 'email', 'groups']
 c.MyOAuth.blocked_users = ['guest', 'test']
 c.MyOAuth.admin_users = ['admins']
 # Create system users just-in-time.
 # c.MyOAuth.create_system_users = True
 
+# Load roles, groups and services
 c.JupyterHub.load_roles = my_config.load_roles()
 c.JupyterHub.load_groups = my_config.load_groups()
 c.JupyterHub.services = my_config.services()
@@ -51,24 +57,19 @@ c.JupyterHub.hub_ip = "jupyterhub"
 c.JupyterHub.hub_port = 8080
 
 # Docker spawner
-# c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.JupyterHub.spawner_class = MyDockerSpawner
-# c.MyDockerSpawner.extra_create_kwargs.update({'user': 'root'})
 
-# c.MyDockerSpawner.config_user(ad_user=ad_user)
-# c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
 
+# The allowed images are defined in config.yaml
 c.MyDockerSpawner.allowed_images = my_config.allowed_images()
 
 # c.JupyterHub.cleanup_servers=False
 c.MyDockerSpawner.network_name = "jupyterhub"
 c.MyDockerSpawner.use_internal_ip = True
-# c.MyDockerSpawner.working_dir = "/home/coder"
-# See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
 
 
-# user data persistence
-# see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
+# The volume mounted inside the container
+# Persist hub data on volume mounted inside container
 notebook_dir_public = "/public"
 notebook_dir_lab_data = "/lab"
 
@@ -82,9 +83,22 @@ c.MyDockerSpawner.volumes = {
             lab_data: {'bind': notebook_dir_lab_data, 'mode': 'ro'},
             local_home: {'bind': jupyter_home, 'mode': 'rw'}
             }
+
+# Others
 c.MyDockerSpawner.remove = True
 c.MyDockerSpawner.debug = False
 c.MyDockerSpawner.host_ip = "0.0.0.0"
+# Extra kwargs for docker
+c.MyDockerSpawner.extra_create_kwargs = {
+    # Define the hostname of the container
+    'hostname': '{username}-{servername}'
+}
+
+
+# Resource Users can use
+c.MyDockerSpawner.cpu_limit = 32.0
+c.MyDockerSpawner.mem_limit = '16G'
+# # if you want to use GPU, uncomment the following lines
 # c.MyDockerSpawner.extra_host_config = {
 #     "device_requests": [
 #         docker.types.DeviceRequest(
@@ -93,23 +107,11 @@ c.MyDockerSpawner.host_ip = "0.0.0.0"
 #         ),
 #     ],
 # }
-# c.MyDockerSpawner.name_template = 'jupyter-{username}-{servername}'.rstrip('-')
-c.MyDockerSpawner.extra_create_kwargs = {
-    'hostname': '{username}-{servername}'
-}
 
-c.ConfigurableHTTPProxy.debug = False
 
-# Other stuff
-c.MyDockerSpawner.cpu_limit = 32.0
-c.MyDockerSpawner.mem_limit = '16G'
 
-# print('allow_images: ', c.MyDockerSpawner.allowed_images)
-
-# Persist hub data on volume mounted inside container
 # c.JupyterHub.cookie_secret_file = "/data/jupyterhub_cookie_secret"
 c.JupyterHub.db_url = "sqlite:////data/jupyterhub.sqlite"
-
 
 # shutdown the server after no activity for an hour
 c.ServerApp.shutdown_no_activity_timeout = 120 * 60
@@ -121,3 +123,6 @@ c.MappingKernelManager.cull_interval = 2 * 60
 
 c.JupyterHub.allow_named_servers = True
 c.JupyterHub.named_server_limit_per_user = 10
+
+
+c.ConfigurableHTTPProxy.debug = False
--- src/config/jupyterhub_config.py
+++ src/config/jupyterhub_config.py
@@ -1,109 +1,117 @@
 import os
 import sys
+
+# The following is necessary
+#  ==============================================================================
 CONFIG_SCRIPT_DIR = os.getenv('CONFIG_SCRIPT_DIR', '/config')
+CONFIG_FILE = os.getenv('CONFIG_FILE', '/etc/jupyterhub/config.yaml')
 sys.path.append(CONFIG_SCRIPT_DIR)
 from MyConfig import MyConfig,my_pre_spawn_hook
 from MyOAuth import MyOAuth
 from MyDockerSpawner import MyDockerSpawner
-
 import docker
+# ==============================================================================
 c = get_config()
 
-my_config=MyConfig(filename='/etc/jupyterhub/config.yaml')
+my_config=MyConfig(filename=CONFIG_FILE)
 
 # Generic
+# c.Application.log_level = 'DEBUG'
 c.JupyterHub.admin_access = True
 c.Spawner.default_url = '/lab'
 
-
-# c.Application.log_level = 'DEBUG'
-# c.JupyterHub.authenticator_class = GenericOAuthenticator
+# Authenticator
 c.JupyterHub.authenticator_class = MyOAuth
 c.MyOAuth.auto_login = True
-c.MyOAuth.discovery_url = "https://auth.xxxxxxx.com/.well-known/openid-configuration"
-c.MyOAuth.client_id = "xxxxxxx"
-c.MyOAuth.client_secret = "xxxxxxx"
-# c.MyOAuth.token_url = "https://auth.xxxxxxx.com//accessToken"
-# c.MyOAuth.userdata_url = "https://auth.xxxxxxx.com/profile"
-# c.MyOAuth.authorize_url = "https://auth.xxxxxxx.com/oidc/authorize"
-# c.MyOAuth.logout_redirect_url = "https://auth.xxxxxxx.com/logout"
-c.MyOAuth.oauth_callback_url = "https://code.xxxxxxx.com/hub/oauth_callback"
-
-c.MyOAuth.username_claim = "preferred_username"
-c.MyOAuth.login_service = 'OIDC'
-
-c.MyOAuth.allowed_groups = ['Member']
-# users with `administrator` role will be marked as admin
-c.MyOAuth.admin_groups = ['Administrators', 'AdminJupyterHub']
-c.MyOAuth.manage_groups = False
+c.MyOAuth.discovery_url = "https://auth.example.com/realms/realm"+"/.well-known/openid-configuration"
+# or (you can add "/.well-known/openid-configuration" or not)
+c.MyOAuth.discovery_url = "https://auth.example.com/realms/realm"
+c.MyOAuth.client_id = "code-oidc"
+c.MyOAuth.client_secret = "CHANGEIT"
 c.MyOAuth.scope = ['openid', 'profile', 'email', 'groups']
+c.MyOAuth.oauth_callback_url = "https://code.example.com/hub/oauth_callback"
+c.MyOAuth.username_claim = "preferred_username"
+c.MyOAuth.login_service = 'Example'
+# If group is not defined, all users will be allowed
+GROUP = ['group1', 'group2', 'admins']
+c.MyOAuth.allowed_groups = GROUP
+# users with `administrator` role will be marked as admin
+c.MyOAuth.admin_groups = ['Administrators', 'AdminJupyterHub','admins']
+c.MyOAuth.manage_groups = False
 c.MyOAuth.blocked_users = ['guest', 'test']
-c.MyOAuth.admin_users = ['admin']
+c.MyOAuth.admin_users = ['admins']
 # Create system users just-in-time.
-c.MyOAuth.create_system_users = True
+# c.MyOAuth.create_system_users = True
 
+# Load roles, groups and services
 c.JupyterHub.load_roles = my_config.load_roles()
 c.JupyterHub.load_groups = my_config.load_groups()
 c.JupyterHub.services = my_config.services()
+
+c.Spawner.pre_spawn_hook = my_pre_spawn_hook
+
 c.JupyterHub.base_url = "/"
-# SSL_DIR = '/ssl'
-# c.JupyterHub.ssl_key = SSL_DIR+"/key.pem"
-# c.JupyterHub.ssl_cert = SSL_DIR+"/fullchain.pem"
 
 # User containers will access hub by container name on the Docker network
 c.JupyterHub.hub_ip = "jupyterhub"
 c.JupyterHub.hub_port = 8080
 
 # Docker spawner
-c.JupyterHub.spawner_class = 'MyDockerSpawner'
-c.Spawner.pre_spawn_hook = my_pre_spawn_hook
-ad_user = my_config.ad()
-c.MyDockerSpawner.config_user(ad_user=ad_user)
-# Other stuff
-c.Spawner.cpu_limit = 30
-c.Spawner.mem_limit = '64G'
-# c.MyDockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
-# c.MyDockerSpawner.allowed_images = my_config.allow_images()
+c.JupyterHub.spawner_class = MyDockerSpawner
+
+
+# The allowed images are defined in config.yaml
+c.MyDockerSpawner.allowed_images = my_config.allowed_images()
 
 # c.JupyterHub.cleanup_servers=False
-c.MyDockerSpawner.network_name = "jupyterhub-net"
+c.MyDockerSpawner.network_name = "jupyterhub"
 c.MyDockerSpawner.use_internal_ip = True
-# c.DockerSpawner.working_dir = "/home/coder"
-# See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
 
-# user data persistence
-# see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
-notebook_dir = '/home/jovyan'
-notebook_dir_public = notebook_dir+"/public"
-# notebook_dir_data = notebook_dir+"/data"
 
-user_dir = 'jupyterhub-{username}'
-public_data = 'jupyterhub-public'
+# The volume mounted inside the container
+# Persist hub data on volume mounted inside container
+notebook_dir_public = "/public"
+notebook_dir_lab_data = "/lab"
 
-c.MyDockerSpawner.notebook_dir = notebook_dir
+lab_data = '/lab'
+public_data = '/public'
+local_home = '/home/users/{username}'
+jupyter_home = '/home/{username}'
+c.MyDockerSpawner.notebook_dir = jupyter_home
 c.MyDockerSpawner.volumes = {
-            user_dir: {'bind': notebook_dir, 'mode': 'rw'},
             public_data: {'bind': notebook_dir_public, 'mode': 'rw'},
+            lab_data: {'bind': notebook_dir_lab_data, 'mode': 'ro'},
+            local_home: {'bind': jupyter_home, 'mode': 'rw'}
             }
+
+# Others
 c.MyDockerSpawner.remove = True
-c.MyDockerSpawner.debug = True
-# c.DockerSpawner.host_ip = "0.0.0.0"
-c.MyDockerSpawner.extra_host_config = {
-    "device_requests": [
-        docker.types.DeviceRequest(
-            count=-1,
-            capabilities=[["gpu"]],
-        ),
-    ],
+c.MyDockerSpawner.debug = False
+c.MyDockerSpawner.host_ip = "0.0.0.0"
+# Extra kwargs for docker
+c.MyDockerSpawner.extra_create_kwargs = {
+    # Define the hostname of the container
+    'hostname': '{username}-{servername}'
 }
 
 
+# Resource Users can use
+c.MyDockerSpawner.cpu_limit = 32.0
+c.MyDockerSpawner.mem_limit = '16G'
+# # if you want to use GPU, uncomment the following lines
+# c.MyDockerSpawner.extra_host_config = {
+#     "device_requests": [
+#         docker.types.DeviceRequest(
+#             count=-1,
+#             capabilities=[["gpu"]],
+#         ),
+#     ],
+# }
 
 
-# Persist hub data on volume mounted inside container
-c.JupyterHub.cookie_secret_file = "/data/jupyterhub_cookie_secret"
+
+# c.JupyterHub.cookie_secret_file = "/data/jupyterhub_cookie_secret"
 c.JupyterHub.db_url = "sqlite:////data/jupyterhub.sqlite"
-
 
 # shutdown the server after no activity for an hour
 c.ServerApp.shutdown_no_activity_timeout = 120 * 60
@@ -114,4 +122,7 @@ c.MappingKernelManager.cull_interval = 2 * 60
 
 
 c.JupyterHub.allow_named_servers = True
-c.JupyterHub.named_server_limit_per_user = 5
+c.JupyterHub.named_server_limit_per_user = 10
+
+
+c.ConfigurableHTTPProxy.debug = False
